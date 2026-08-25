a5ca5ce6add015db87a2b1231ead58da10415fff
Teesha Matarage <tmatarage01@gmail.com>
2026-08-25 21:23:27 +0530

    Hand detection enabled

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
 import cv2
+import mediapipe as mp
+
+mp_hands = mp.solutions.hands
+mp_drawing = mp.solutions.drawing_utils
+
+hands = mp_hands.Hands(
+    static_image_mode = False,
+    max_num_hands = 1,
+    min_detection_confidence = 0.5,
+    min_tracking_confidence = 0.5
+)
 
 cap = cv2.VideoCapture(0)
 
@@ -9,9 +20,27 @@ while True:
         print("Could not access camera!")
         break
 
-    cv2.imshow("Camera Test", frame)
-    if cv2.waitKey(1) & 0xFF == ord("q"):
+    #BGR to RGB
+    rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+    #hand detection
+    results = hands.process(rgb_frame)
+
+    #Draw landmarks by hand detection
+    if results.multi_hand_landmarks:
+        for hand_landmarks in results.multi_hand_landmarks:
+            mp_drawing.draw_landmarks(
+                frame,
+                hand_landmarks,
+                mp_hands.HAND_CONNECTIONS
+            )
+
+    cv2.imshow("SkyWriting.ap - Hand Detection", frame)
+
+    #Press 'e' to exit the webcam
+    if cv2.waitKey(1) & 0xFF == ord("e"):
         break
 
 cap.release()
 cv2.destroyAllWindows()
+hands.close()
